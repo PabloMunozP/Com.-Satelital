2759d13ad1aa2015aae09a07761988ce7b7f82bb
PabloMunozP <41455709+PabloMunozP@users.noreply.github.com>
2021-09-14 22:48:46 -0300

    Update Tarea1.py
    Angulo de elevacion

--- Tarea 1/Tarea1.py
+++ Tarea 1/Tarea1.py
@@ -1,86 +1,39 @@
-import math
+# Desarrolle un programa que al ingresar la latitud y longitud en formato decimal de una estación terrena,
+#  la latitud y longitud del punto del subsatélite en formato decimal de un satélite GEO y 
+# la altura del satélite (en KM ) determine el ángulo de elevación, el Azimut y validar si hay o no visibilidad.
 
-def seno(grados):# para aceptar grados y retornar el seno
-    return math.sin(math.radians(grados))
-
-def coseno(grados):# para aceptar grados y retornar el coseno
-    return math.cos(math.radians(grados))
-
-def arccos(grados):
-    return math.degrees(math.acos(math.radians(grados)))
-
-def arcseno(grados):
-    return math.degrees(math.asin(math.radians(grados)))
-
-def arctan(grados):
-    return math.degrees(math.atan(math.radians(grados)))
-
-def angulo_gamma(Lt,lt,Ls,ls):
-    #Lt: Latitud terrena // Ls: Latitud satelite
-    #lt: longitud terrena // ls: longitud satelite
-    result=coseno(Lt)*coseno(Ls)*coseno(ls-lt) + seno(Lt)*seno(Ls)
-    return arccos(result)
-
-def angulo_elevacion(gamma,radioSatelite,radioTierra):
-
-    result= seno(gamma)/math.pow((1+math.pow((radioTierra/radioSatelite),2) - 2*(radioTierra/radioSatelite)*coseno(gamma) ), 0.5)
-    return arccos(result)
-
-def angulo_alfa(Lt,lt,ls):
-    #Lt: Latitud terrena // Ls: Latitud satelite
-    #lt: longitud terrena // ls: longitud satelite
-    result= (math.tan(math.radians(abs(ls-lt)))) / seno(abs(Lt))
-    return arctan(result)
-
-def azimut(Lt,lt,Ls,ls):
-     #Lt: Latitud terrena // Ls: Latitud satelite
-    #lt: longitud terrena // ls: longitud satelite
-    alfa=angulo_alfa(Lt,lt,ls)
-    # valores Latitud: -90 a 90
-    # valores longitud: -180 a 180
-    #Revisar posicion terrena
-    if Lt>0: # Hemisferio Norte
-        
-        #El satelite esta mas al norte que la estacion terrena
-        if Ls>Lt:
-            return -1
-        #Satelite al SE de la estacion terrena
-        elif ls>lt:
-            return 180 - alfa
-        #Satelite al SO de la estacion terrena
-        elif ls<lt:
-            return 180 + alfa
-
-    elif Lt<0: # Hemisferio sur
-        
-        #Satelite al Sur de la estacion terrena
-        if Ls<Lt:
-            return -1
-        #Satelite al NE
-        elif ls>lt : 
-            return  alfa
-        elif ls<lt:
-            return 360 - alfa
+import math as m
 
 
-# valores Latitud: -90 a 90
-# valores longitud: -180 a 180
-print('''A continuacion se le solicitan los datos para saber cada ubicacion necesaria. Por favor ingrese los valores en formato decimal utilizando un punto.\n 
-        Solo se aceptaran valores entre -90 y 90 para latitud y entre -180 y 180 para longitud. \n En caso de ingresar un formato incorrecto no se podra calcular.''')
+def elevacion(Le,le,Ls,ls,rs):
+    # Recibe latitud y longitud en angulos 
+    # Le: latitud estación terrena.
+    # le: longitud estación terrena
+    # Ls: latitud satélite
+    # ls: longitud satélite
+    Le=Le * m.pi/180
+    le=le* m.pi/180
+    Ls=Ls* m.pi/180
+    ls=ls* m.pi/180
 
-T_latitud=input('Ingrese la latitud de la estacion terrena: ').split(' ',1)[0]
-T_longitud = input(' Ingrese la longitud de la estacion terrena: ').split(' ',1)[0]
-S_latitud= input('Ingrese la latitud del Satelite: ').split(' ',1)[0]
-S_longitud = input(' Ingrese la longitud del Satelite: ').split(' ',1)[0]
-altura= input(' Ingrese la altura del Satelite: ').split(' ',1)[0]
+    print(Le,le,Ls,ls)
+    
+    #Calcular gamma segun la ecuacion 1
+    # 1.- cos(gamma) = cos(Le)*cos(Ls)*cos(ls-le)+sin(Le)*sin(Ls)
+    cos_gamma = (m.cos(Le) * m.cos(Ls) * m.cos(ls-le)) + ( m.sin(Le)*m.sin(Ls) )
+    gamma=m.acos(cos_gamma)#en radian
+
+    print('coseno: ', cos_gamma, 'gamma: ', gamma )
+
+    #radio tierra
+    re= 6378.137 #KM
+    #Calcular elevacion segun ecuacion 2
+    #2.- cos(El) = sin(gamma) / (1+ (re/rs)**2 - 2(re/rs)*cos_gamma)**2
+    cos_elev = m.sin(gamma) / ( 1 + (re/rs)**2 - (2*(re/rs) * m.cos(gamma)) )**0.5 
+    elev=m.acos(cos_elev)
+    print('coseno: ', cos_elev, 'elev ', elev )
+
+    print('grados:', m.degrees(elev))
 
 
-#Comprobar que los angulos dados esten en el rango aceptado o formatearlos
-
-
-#Calcular el angulo de elevacion y azimut
-
-#Ver si hay visibilidad entre el satelite y el observador
-
-print(T_latitud,T_longitud,S_latitud,S_longitud,altura)
-
+elevacion(-33.42,-70.56,-0.08,98.06,35789.91)
