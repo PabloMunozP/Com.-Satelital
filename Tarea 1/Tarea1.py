0200911a3746fdbfa1c0438ec3589071f9fb539b
PabloMunozP <41455709+PabloMunozP@users.noreply.github.com>
2021-09-22 22:11:29 -0300

    Update Tarea1.py

--- Tarea 1/Tarea1.py
+++ Tarea 1/Tarea1.py
@@ -16,6 +16,9 @@ def elevacion(Le,le,Ls,ls,rs):
     Ls=Ls* m.pi/180
     ls=ls* m.pi/180
 
+    #radio tierra
+    re= 6370 #KM
+    rs+=re
     print(Le,le,Ls,ls)
     
     #Calcular gamma segun la ecuacion 1
@@ -23,52 +26,58 @@ def elevacion(Le,le,Ls,ls,rs):
     # cos_gamma = (m.cos(Le) * m.cos(Ls) * m.cos(ls-le)) + ( m.sin(Le)*m.sin(Ls) )
     gamma=m.acos(m.cos(Le) * m.cos(Ls) * m.cos(ls-le) +  m.sin(Le)*m.sin(Ls) )#en radian
     # gamma = np.arccos( np.cos(np.deg2rad(Le))*np.cos(np.deg2rad(Ls))*np.cos(np.deg2rad(ls-le)) + np.sin(np.deg2rad(Le))*np.sin(np.deg2rad(Ls)) )
-
-
     # print('gamma: ', m.degrees(gamma))
 
-    #radio tierra
-    re= 6378.137 #KM
-    #Calcular elevacion segun ecuacion 2
-    #2.- cos(El) = sin(gamma) / (1+ (re/rs)**2 - 2(re/rs)*cos_gamma)**2
-    # cos_elev = m.sin(gamma) / ( 1 + (re/rs)**2 - (2*(re/rs) * m.cos(gamma)) )**0.5 
-    result= (m.sin(gamma) / m.sqrt( 1 + (re/rs)**2 - (2*(re/rs) * m.cos(gamma)) ))
-    elev=m.acos(m.sin(gamma) / m.sqrt( 1 + (re/rs)**2 - (2*(re/rs) * m.cos(gamma)) ))
-    # elevation = np.arccos( np.sin(np.deg2rad(gamma)) / np.sqrt(1+(6378.137/rs)**(2) -2*(6378.137/rs)*np.cos(np.deg2rad(gamma))) )
-    # print('elevacion con acos: ', elev )
-    # print('elevacion antes acos:', result)
+    if m.degrees(gamma) > 81.3:
+        print('El satelite no es visible')
+        elev=m.acos(m.sin(gamma) / m.sqrt( 1 + (re/rs)**2 - (2*(re/rs) * m.cos(gamma)) ))
+        # elevation = np.arccos( np.sin(np.deg2rad(gamma)) / np.sqrt(1+(6378.137/rs)**(2) -2*(6378.137/rs)*np.cos(np.deg2rad(gamma))) )
+        # print('elevacion con acos: ', elev )
 
-    print('grados:', m.degrees(elev))
+        print('elevacion en grados:', m.degrees(-elev))
+        azimut(Le,le,ls,gamma)
 
-def azimut(Le,le,Ls,ls):
-    Le=Le * m.pi/180
-    le=le* m.pi/180
-    Ls=Ls* m.pi/180
-    ls=ls* m.pi/180
+        
+    else:
+        print('el satelite es visible')
+        
+        #Calcular elevacion segun ecuacion 2
+        #2.- cos(El) = sin(gamma) / (1+ (re/rs)**2 - 2(re/rs)*cos_gamma)**2
+        # cos_elev = m.sin(gamma) / ( 1 + (re/rs)**2 - (2*(re/rs) * m.cos(gamma)) )**0.5
+        
+        elev=m.acos(m.sin(gamma) / m.sqrt( 1 + (re/rs)**2 - (2*(re/rs) * m.cos(gamma)) ))
+        # elevation = np.arccos( np.sin(np.deg2rad(gamma)) / np.sqrt(1+(6378.137/rs)**(2) -2*(6378.137/rs)*np.cos(np.deg2rad(gamma))) )
+        # print('elevacion con acos: ', elev )
 
-    #print(Le,le,Ls,ls)
+        print('elevacion en grados:', m.degrees(elev))
 
-    B = ls - le
+def azimut(Le,le,ls,gamma):
+    
+    alpha = m.atan(m.tan(abs(ls-le))/m.sin(Le))
+    # print(alpha,'   debe ser entre 0 y pi')
 
-    alpha = m.atan(m.tan(abs(B))/m.sin(Le))
+    if m.degrees(gamma)>81.3:#El satelite no es visible
+        alpha += m.pi # borrar e ir probando 
+        # print(alpha,'   debe ser entre 0 y pi')
+    
+    
 
-    alpha += m.pi # borrar e ir probando 
+    if Le < 0 :
+        az = abs(alpha)
+        # print(az*180/m.pi)
+    if Le < 0 :
+        az = 2*m.pi - abs( alpha)
+        # print(az*180/m.pi)
+    if Le > 0:
+        az = m.pi + abs(alpha)
+        # print(az*180/m.pi)
+    if Le > 0:
+        az = m.pi - abs( alpha)
+        # print(az*180/m.pi)
 
-    # print(B)
-    # print(alpha)
+    print('azimut: ' , m.degrees(az))
+    
 
-    if Le < 0 and B < 0:
-        az = alpha
-        print(az*180/m.pi)
-    if Le < 0 and B > 0:
-        az = 2*m.pi - alpha
-        print(az*180/m.pi)
-    if Le > 0 and B < 0:
-        az = m.pi + alpha
-        print(az*180/m.pi)
-    if Le > 0 and B > 0:
-        az = m.pi - alpha
-        print(az*180/m.pi)
 
 
 
@@ -81,6 +90,27 @@ def azimut(Le,le,Ls,ls):
 # elevacion(-33.03932,-71.62725,7.75,-169.55,35991.82)#-19.9 da 20.39
 
 
-elevacion(-32.83369,-70.59827,-0.18,87.40,35771.53)
+# elevacion(-32.83369,-70.59827,-0.18,87.40,35771.53)
 
-azimut(-32.83369,-70.59827,-0.18,87.40)
+
+# elevacion(-33.42628,-70.59827,0.04,-100.86,35787.76)
+# azimut(-33.42628,-70.59827,0.04,-100.86)
+
+# elevacion(-32.83369,-70.59827,-0.99,-68.58,35708.96)
+# azimut(-32.83369,-70.59827,-0.99,-68.58)
+
+
+if __name__== "__main__":
+    Le,le =  -32.83369,-70.59827
+    Ls,ls = -0.02,61.94
+    rs= 35785.90
+
+    flag= True if (Le>90 or Le<-90) or (Ls>90 or Ls<-90) or (le>180 or le<-180) or (ls>180 or  ls<-180) else False
+
+    while flag:
+        print('hubo un error en los datos ingresados')
+        flag=False
+    
+
+    elevacion(Le,le,Ls,ls,rs)
+
